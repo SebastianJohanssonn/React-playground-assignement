8e604f65de4e1f81356f8e90536423dfde5c3f57
Sebastian Johansson <sebben.97@hotmail.com>
2019-04-22 13:21:26 +0200

    added searchbar to DOM

--- src/components/viewContainer/viewContainer.tsx
+++ src/components/viewContainer/viewContainer.tsx
@@ -1,6 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, CSSProperties } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Spinner from '../spinner';
+import SearchBar from "./searchBar";
 
 const MasterView = React.lazy(() => import(/* webpackChunkName: "masterView" */ './masterView'));
 const DetailView = React.lazy(() => import(/* webpackChunkName: "detailView" */ './detailView/detailView'));
@@ -8,18 +9,21 @@ const DetailView = React.lazy(() => import(/* webpackChunkName: "detailView" */ 
 /** React function component */
 export default function ViewContainer() {
 
-    const detailViews = ['forest', 'sky', 'desert'];
+    const view = "";
 
     return (
         <Suspense fallback={<Spinner/>}>
             <Switch>
-                <Route exact path="/" render={() =>
-                    <MasterView detailViews={detailViews}/>
-                }/>
-                    <Route path="/forest" component={DetailView}/>
-                    <Route path="/sky" component={DetailView}/>
-                    <Route path="/desert" component={DetailView}/>
+                <Route exact path="/:id" component={DetailView}/>
+                <div style={centered}>
+                    <SearchBar />
+                </div>
             </Switch>
         </Suspense>
     );
+}
+const centered: CSSProperties = {
+    height: '100%',
+    textAlign: 'center',
+    marginTop: '5em'
 }
